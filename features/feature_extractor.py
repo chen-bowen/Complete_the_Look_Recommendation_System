9ba7b9fb1b13bef4d5c1c3d199ad6c417d3999cf
oscar <oparrilla96@gmail.com>
2021-12-05 12:03:33 -0500

    import pickle, stack to cat

--- features/feature_extractor.py
+++ features/feature_extractor.py
@@ -5,6 +5,7 @@ import torchvision
 import tqdm
 from dataset.create_dataloader import dataloader
 from torchvision import models
+import pickle
 
 IMAGE_TYPE = "product"
 def extractor():
@@ -21,16 +22,14 @@ def extractor():
     resnet.eval()
     transforms = torchvision.transforms.Resize(256)
     all_features = []
-
     for batch in tqdm.tqdm(data_loader, total=len(data_loader)):
         X = transforms(batch)  # resizes to 256 X 256 for ResNet
         X = X.float()
         with torch.no_grad():
             batch_features = resnet(X)
             all_features.append(batch_features)
-
-    all_features = torch.stack(all_features)
-    breakpoint()
+    all_features = torch.cat(all_features)
+    
     with open(f'{IMAGE_TYPE}_features.pickle', 'wb') as handle:
         pickle.dump(all_features, handle, protocol=pickle.HIGHEST_PROTOCOL)
     return all_features
