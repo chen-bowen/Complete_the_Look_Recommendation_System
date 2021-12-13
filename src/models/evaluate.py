02eaecd844ef516d1300257cf24e669404ad3423
oscar <oparrilla96@gmail.com>
2021-12-13 02:41:45 +0000

    changes to evaluate.py

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,12 +1,14 @@
 import pickle
+import pandas as pd
 from utils.similarity import calculate_similarity
 
 def evaluation():
     with open("features/compatible_product_embedding.pickle", "rb") as f:
         features = pickle.load(f)
+    metadata = pd.read_csv("dataset/metadata/dataset_metadata_ctl_single.csv")
+    sim = calculate_similarity(features[0],features[1], "cosine")
     breakpoint()
-    calculate_similarity()
-    pass
+    
 
 if __name__ == "__main__":
     evaluation()
